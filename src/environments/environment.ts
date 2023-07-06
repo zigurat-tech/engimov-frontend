b2ce7b8be5f1a95c327211255315cf782a518668
Gilbert Veitia Urbay <gveitia13@gmail.com>
2023-07-06 04:20:31 -0400

    initial

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBcgEIv-155gCQyT1uDdgJBqzjrvSWO-LY",
+    authDomain: "tarjetacredito-43cd1.firebaseapp.com",
+    projectId: "tarjetacredito-43cd1",
+    storageBucket: "tarjetacredito-43cd1.appspot.com",
+    messagingSenderId: "593614495826",
+    appId: "1:593614495826:web:058a430309e455bed4ff8b"
+  },
+  url: 'http://127.0.0.1:8000/'
+};
