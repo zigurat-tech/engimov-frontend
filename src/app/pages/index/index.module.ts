01ad8cc153e152a0bc00495b5aacb18888371766
Gilbert Veitia Urbay <gveitia13@gmail.com>
2023-07-21 18:38:09 -0400

    funciona el index

--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -1,17 +1,28 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { IndexRoutingModule } from './index-routing.module';
-import { IndexComponent } from './components/index/index.component';
+import {IndexRoutingModule} from './index-routing.module';
+import {IndexComponent} from './components/index/index.component';
+import {AboutComponent} from "@app/components/about/about.component";
+import {ContactComponent} from "@app/components/contact/contact.component";
+import {LoaderComponent} from "@app/components/shared/loader/loader.component";
 
 
 @NgModule({
   declarations: [
-    IndexComponent
+    IndexComponent,
+    AboutComponent,
+    ContactComponent,
+    LoaderComponent,
   ],
   imports: [
     CommonModule,
-    IndexRoutingModule
-  ]
+    IndexRoutingModule,
+  ],
+  exports: [
+    AboutComponent,
+    ContactComponent
+  ],
 })
-export class IndexModule { }
+export class IndexModule {
+}
