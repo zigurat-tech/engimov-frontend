0a9ecce912fcb211958376c9cb46a9ec474fbab2
Gilbert Veitia Urbay <61420914+gveitia13@users.noreply.github.com>
2023-11-12 03:18:31 -0500

    update endpoints

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -8,7 +8,8 @@ export const environment = {
     messagingSenderId: "593614495826",
     appId: "1:593614495826:web:058a430309e455bed4ff8b"
   },
-  // url: 'https://engimov.onrender.com/api'
-  url: 'http://127.0.0.1:8000/api',
-  basic_url: 'http://127.0.0.1:8000',
+  // url: 'http://127.0.0.1:8000/api',
+  url: 'http://127.0.0.1:15051/api',
+  basic_url: 'http://127.0.0.1:15051',
+  // basic_url: 'http://127.0.0.1:8000',
 };
